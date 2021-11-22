6556c13605bc82792f26b047ddea613b53edac31
Ismael-Vilacian <ismaelv161@gmail.com>
2021-11-22 20:08:24 -0300

    Feito as importações e adicionado os métodos

--- Server.java
+++ Server.java
@@ -1,3 +1,48 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.Socket;
+import java.util.ArrayList;
+
 public class Server {
-    
+    private static ArrayList<BufferedWriter> clientes;
+    private String nome;
+    private Socket con;
+    private BufferedReader bfr;
+
+    public void run() {
+
+        try {
+            String msg;
+            OutputStream ou = this.con.getOutputStream();
+            Writer ouw = new OutputStreamWriter(ou);
+            BufferedWriter bfw = new BufferedWriter(ouw);
+            clientes.add(bfw);
+            nome = msg = bfr.readLine();
+
+            while (!"Sair".equalsIgnoreCase(msg) && msg != null) {
+                msg = bfr.readLine();
+                sendToAll(bfw, msg);
+                System.out.println(msg);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendToAll(BufferedWriter bwSaida, String msg) throws IOException {
+        BufferedWriter bwS;
+
+        for (BufferedWriter bw : clientes) {
+            bwS = (BufferedWriter) bw;
+            if (!(bwSaida == bwS)) {
+                bw.write(nome + " -> " + msg + "\r\n");
+                bw.flush();
+            }
+        }
+    }
 }
